3827b1239c06e8f788b77af610c7a91f93c668f9
Ubuntu <ubuntu@ip-172-31-85-248.ec2.internal>
2020-05-21 19:56:54 +0000

    index

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContributionsComponent } from './contributions/contributions.component';
+import { CommentsComponent } from './comments/comments.component';
+import { UsersComponent } from './users/users.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ContributionsComponent,
+    CommentsComponent,
+    UsersComponent
+
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+
+
