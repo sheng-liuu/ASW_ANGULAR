00ff892c5d2068d9e57e1f641b94e8f229dd1db4
Ubuntu <ubuntu@ip-172-31-85-248.ec2.internal>
2020-05-22 22:57:40 +0000

    comment_details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { SubmittedComponent } from './submitted/submitted.component';
 import { ThreadsComponent } from './threads/threads.component';
 import { UpvotedComponent } from './upvoted/upvoted.component';
+import { CommentDetailsComponent } from './comment-details/comment-details.component';
 
 export function socialConfigs() {  
   const config = new AuthServiceConfig(  
@@ -33,6 +34,7 @@ export function socialConfigs() {
     SubmittedComponent,
     ThreadsComponent,
     UpvotedComponent,
+    CommentDetailsComponent,
   ],
   imports: [
     BrowserModule,
