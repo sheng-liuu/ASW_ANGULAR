caf58b042c2db7947d7f0b6b080a9bcf6fdd957c
ariadna <ariadnavc98@gmail.com>
2020-05-27 18:03:01 +0200

    Cambios contribution

--- src/app/service/contribution.service.ts
+++ src/app/service/contribution.service.ts
@@ -41,7 +41,18 @@ export class ContributionService {
     return this.http.get<Contribution[]>(
     'https://hackernews12c.herokuapp.com/api/v1/user/upvoted', httpOptions);
   }
-  
+  getAsk(): Observable<Contribution[]> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'X-API-KEY': localStorage.getItem("apikey"),
+        'Accept': 'application/json'
+      })
+    };
+    
+    return this.http.get<Contribution[]>(
+    'https://hackernews12c.herokuapp.com/api/v1/contributions?nametype=ask', httpOptions);
+  }
   getContribution(id): Observable<Contribution> {
     const httpOptions = {
       headers: new HttpHeaders({
