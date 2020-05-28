091b6db6031642f7e8bafb2fc0550491a20899ca
EC2 Default User <ec2-user@ip-172-31-76-190.ec2.internal>
2020-05-28 23:14:08 +0000

    make it better

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/*const express = require('express');
 const path = require('path');
 
 const app = express();
@@ -9,6 +9,23 @@ app.use(express.static(__dirname + '/dist'));
 app.get('/*', function(req,res) {
     
 res.sendFile(path.join(__dirname+'/app.component.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);*/
+
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
