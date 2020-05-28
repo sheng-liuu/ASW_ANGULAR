1fddebfe0fe20312d37e8f0734b1ed927480b0bb
EC2 Default User <ec2-user@ip-172-31-76-190.ec2.internal>
2020-05-28 22:51:33 +0000

    make it better

--- server.js
+++ server.js
@@ -1,25 +1,15 @@
-const express = require('express')
-const app = express();
+const express = require('express');
 const path = require('path');
 
-app.use(express.static(__dirname + '/hackernews-angular'));
+const app = express();
 
-app.listen(process.env.PORT || 8080);
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist'));
 
-// PathLocationStrategy
-app.get('/*', function(req, resp) {
-    resp.sendFile(path.join(__dirname + '/hackernews-angular/index.html'));
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
-console.log('Console listening');
-
-/*var express = require('express')
-var path = require('path')
-var serveStatic = require('serve-static')
-
-var app = express()
-app.use(serveStatic(path.join(__dirname, 'dist')))
-
-var port = process.env.PORT || 8000
-app.listen(port)
-console.log('server started ' + port)*/
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
