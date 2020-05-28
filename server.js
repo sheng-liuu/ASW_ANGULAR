c4857ad527927c62496b1219c444603073cc51b5
EC2 Default User <ec2-user@ip-172-31-76-190.ec2.internal>
2020-05-28 22:44:10 +0000

    make it better

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express')
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist/hackernews-angular'));
+app.use(express.static(__dirname + '/hackernews-angular'));
 
 app.listen(process.env.PORT || 8080);
 
 // PathLocationStrategy
 app.get('/*', function(req, resp) {
-    resp.sendFile(path.join(__dirname + '/dist/hackernews-angular/index.html'));
+    resp.sendFile(path.join(__dirname + '/hackernews-angular/index.html'));
 });
 
 console.log('Console listening');
